90599e08698cac2aa99e50c899d82fc5740f8fa2
Nepxion <1394997@qq.com>
2017-12-11 18:06:17 +0800

    提交skeleton-rest模块

---
+++ skeleton-rest/src/main/java/com/nepxion/skeleton/service/SkeletonService.java
@@ -0,0 +1,17 @@
+package com.nepxion.skeleton.service;
+
+/**
+ * <p>Title: Nepxion Skeleton</p>
+ * <p>Description: Nepxion Skeleton For Freemarker</p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+import com.nepxion.skeleton.property.SkeletonProperties;
+
+public interface SkeletonService {
+    void generator(String path, SkeletonProperties skeletonProperties) throws Exception;
+}
