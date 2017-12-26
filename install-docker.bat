35ba9bd73e2aa3284e241a7dad125a20622b7465
Nepxion <1394997@qq.com>
2017-12-26 11:37:18 +0800

    重构Bat

--- install-docker.bat
+++ install-docker.bat
@@ -15,33 +15,34 @@
 @title Nepxion Skeleton
 @color 0a
 
+@set DEPENDENCY_LIST=skeleton-engine
 @set PROJECT_NAME=skeleton-spring-cloud
 @set IMAGE_NAME=skeleton-spring-cloud
+@set MACHINE_PORT=2222
+@set CONTAINER_PORT=2222
 
-@rem 删除target，有时候mvn会clean失败，需事先强制删除target
 if exist %PROJECT_NAME%\target rmdir /s/q %PROJECT_NAME%\target
 
-@rem 执行相关模块的install
-call mvn clean install -DskipTests -pl skeleton-engine,%PROJECT_NAME% -am
+@rem 执行相关模块的Maven Install
+call mvn clean install -DskipTests -pl %DEPENDENCY_LIST% ,%PROJECT_NAME% -am
 
-@rem 停止和删除容器
+@rem 停止和删除Docker容器
 call docker stop %IMAGE_NAME%
 @rem call docker kill %IMAGE_NAME%
 call docker rm %IMAGE_NAME%
 
-@rem 删除镜像
+@rem 删除Docker镜像
 call docker rmi %IMAGE_NAME%
 
 cd %PROJECT_NAME%
 
-@rem 安装容器镜像
 set DOCKER_HOST=tcp://localhost:2375
-@rem set DOCKER_HOST=tcp://192.168.99.100:2376
 @rem set DOCKER_CERT_PATH=C:\Users\Neptune\.docker\machine\certs
 
+@rem 安装Docker镜像
 call mvn package docker:build -DskipTests
-@rem call mvn package docker:build -DskipTests && java -jar target\%PROJECT_NAME%-1.0.0.jar
 
-call docker run -i -t -p 2222:2222 --name %IMAGE_NAME% %IMAGE_NAME%:latest
+@rem 安装和启动Docker容器，并自动执行端口映射
+call docker run -i -t -p %MACHINE_PORT%:%CONTAINER_PORT% --name %IMAGE_NAME% %IMAGE_NAME%:latest
 
 pause
