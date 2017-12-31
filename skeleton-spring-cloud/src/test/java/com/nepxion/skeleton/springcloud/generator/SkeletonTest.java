a52bf5a637887cb208baa3afc4b4e6acb4f6f90f
Nepxion <1394997@qq.com>
2017-12-31 10:55:46 +0800

    修改测试类

--- skeleton-spring-cloud/src/test/java/com/nepxion/skeleton/springcloud/generator/SkeletonTest.java
+++ skeleton-spring-cloud/src/test/java/com/nepxion/skeleton/springcloud/generator/SkeletonTest.java
@@ -24,7 +24,12 @@ public class SkeletonTest {
             String generatePath = SkeletonUtil.getTempGeneratePath();
             // String generatePath = "E:/Download/skeleton/";
 
-            // 如果prefixTemplatePath和reducedTemplatePath同时为null，那么Generator类目录和Template目录必须完全一致
+            // 如何理解prefixTemplatePath和reducedTemplatePath含义？
+            // FreeMarker规定，模板文件必须放在classpath下，即以com/...开头的路径为其classpath
+            // 1. 有需求要求，模板文件移动到resources中，并希望放在template/com/a/b/c...，那么template就是prefixTemplatePath，模板的前置路径
+            // 2. 有需求要求，模板文件路径如果template/com/a/b/c/...，觉得路径太长，那么a/b/c/就是reducedTemplatePath，模板路径缩减，把这部分裁剪掉           
+            // 3. 如果把模板文件和Generator类放在一起，则prefixTemplatePath和reducedTemplatePath同时为null即可
+
             // 模板文件所在的前置路径
             String prefixTemplatePath = "template";
             // String prefixTemplatePath = null;
