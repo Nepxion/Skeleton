9eb8b33dd9a30572e03feac5ce032374b78f9f83
Nepxion <1394997@qq.com>
2017-12-31 14:55:53 +0800

    增加模板文件和生成类

--- skeleton-spring-cloud/src/main/java/com/nepxion/skeleton/springcloud/service/ClientProjectServiceImpl.java
+++ skeleton-spring-cloud/src/main/java/com/nepxion/skeleton/springcloud/service/ClientProjectServiceImpl.java
@@ -16,6 +16,12 @@ import com.nepxion.skeleton.engine.exception.SkeletonException;
 import com.nepxion.skeleton.engine.property.SkeletonProperties;
 import com.nepxion.skeleton.framework.service.SkeletonService;
 import com.nepxion.skeleton.springcloud.generator.client.PomXmlGenerator;
+import com.nepxion.skeleton.springcloud.generator.client.java.ClientApplicationClassGenerator;
+import com.nepxion.skeleton.springcloud.generator.client.java.ClientContextAwareClassGenerator;
+import com.nepxion.skeleton.springcloud.generator.client.java.ClientControllerClassGenerator;
+import com.nepxion.skeleton.springcloud.generator.client.java.ClientServiceClassGenerator;
+import com.nepxion.skeleton.springcloud.generator.client.resources.ApplicationPropertiesGenerator;
+import com.nepxion.skeleton.springcloud.generator.shared.resources.LogbackXmlGenerator;
 
 import freemarker.template.TemplateException;
 
@@ -23,6 +29,25 @@ public class ClientProjectServiceImpl implements SkeletonService {
     @Override
     public void generate(String generatePath, String prefixTemplatePath, String reducedTemplatePath, SkeletonProperties skeletonProperties) throws SkeletonException, TemplateException, IOException {
         String projectType = "client";
+        String dependencyProjectType = "server";
+
+        // 创建Java类文件到main/java目录下
+        new ClientApplicationClassGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+
+        // 创建Java类文件到main/java目录下
+        new ClientContextAwareClassGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+
+        // 创建Java类文件到main/java目录下
+        new ClientControllerClassGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+
+        // 创建Java类文件到main/java目录下
+        new ClientServiceClassGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+
+        // 创建文件到main/resources目录下
+        new ApplicationPropertiesGenerator(generatePath, projectType, dependencyProjectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+
+        // 创建文件到main/resources目录下
+        new LogbackXmlGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
 
         // 创建文件到根目录下
         new PomXmlGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
