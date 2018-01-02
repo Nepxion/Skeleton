5efa8a17d7269bf5a4b0a9ff0d473e9738195610
Nepxion <1394997@qq.com>
2018-01-02 11:51:04 +0800

    重构参数传导方式

--- skeleton-spring-cloud/src/test/java/com/nepxion/skeleton/springcloud/generator/SkeletonTest.java
+++ skeleton-spring-cloud/src/test/java/com/nepxion/skeleton/springcloud/generator/SkeletonTest.java
@@ -11,45 +11,49 @@ package com.nepxion.skeleton.springcloud.generator;
  */
 
 import com.nepxion.skeleton.engine.constant.SkeletonConstant;
+import com.nepxion.skeleton.engine.context.SkeletonContext;
 import com.nepxion.skeleton.engine.property.SkeletonProperties;
 import com.nepxion.skeleton.engine.util.SkeletonUtil;
 import com.nepxion.skeleton.framework.service.SkeletonService;
 import com.nepxion.skeleton.springcloud.service.SkeletonServiceImpl;
 
 public class SkeletonTest {
-    public static void main(String[] args) {
-        try {
-            // 创建文件的输出的路径
-            // 放在操作系统的临时目录下
-            String generatePath = SkeletonUtil.getTempGeneratePath();
-            // String generatePath = "E:/Download/skeleton/";
+    public static void main(String[] args) throws Exception {
+        // 创建文件的输出的路径
+        // 放在操作系统的临时目录下
+        String generatePath = SkeletonUtil.getTempGeneratePath();
+        // String generatePath = "E:/Download/skeleton/";
 
-            // 如何理解prefixTemplatePath和reducedTemplatePath含义？
-            // FreeMarker规定，模板文件必须放在classpath下，即以com/...开头的路径为其classpath
-            // 1. 有需求要求，模板文件移动到resources中，并希望放在template/com/a/b/c...，那么template就是prefixTemplatePath，模板的前置路径
-            // 2. 有需求要求，模板文件路径如果template/com/a/b/c/...，觉得路径太长，那么a/b/c/就是reducedTemplatePath，模板路径缩减，把这部分裁剪掉           
-            // 3. 如果把模板文件和Generator类放在一起，则prefixTemplatePath和reducedTemplatePath同时为null即可
+        // ********** 构建全局上下文对象 **********
+        // 如何理解prefixTemplatePath和reducedTemplatePath含义？
+        // FreeMarker规定，模板文件必须放在classpath下，即以com/...开头的路径为其classpath
+        // 1. 有需求要求，模板文件移动到resources中，并希望放在template/com/a/b/c...，那么template就是prefixTemplatePath，模板的前置路径
+        // 2. 有需求要求，模板文件路径如果template/com/a/b/c/...，觉得路径太长，那么a/b/c/就是reducedTemplatePath，模板路径缩减，把这部分裁剪掉       
+        // 3. 如果把模板文件和Generator类放在一起，则prefixTemplatePath和reducedTemplatePath同时为null即可
 
-            // 模板文件所在的前置路径
-            String prefixTemplatePath = "template";
-            // String prefixTemplatePath = null;
+        // 模板文件所在的前置路径
+        String prefixTemplatePath = "template";
+        // String prefixTemplatePath = null;
 
-            // 模板路径缩减
-            String reducedTemplatePath = "com/nepxion/skeleton/springcloud/generator/";
-            // String reducedTemplatePath = null;
+        // 模板路径缩减
+        String reducedTemplatePath = "com/nepxion/skeleton/springcloud/generator/";
+        // String reducedTemplatePath = null;
 
-            // 描述规则的配置文件所在的路径
-            // 配置文件含中文，stringEncoding必须为GBK，readerEncoding必须为UTF-8，文本文件编码必须为ANSI
-            String propertiesPath = "config/skeleton-data.properties";
+        // 全局上下文对象
+        SkeletonContext skeletonContext = new SkeletonContext(prefixTemplatePath, reducedTemplatePath);
+        // **************************************
 
-            // 构造全局配置文件对象
-            SkeletonProperties skeletonProperties = new SkeletonProperties(propertiesPath, SkeletonConstant.ENCODING_GBK, SkeletonConstant.ENCODING_UTF_8);
+        // ********** 构建全局配置类对象 **********
+        // 描述规则的配置文件所在的路径
+        // 配置文件含中文，stringEncoding必须为GBK，readerEncoding必须为UTF-8，文本文件编码必须为ANSI
+        String propertiesPath = "config/skeleton-data.properties";
 
-            // 输出脚手架文件
-            SkeletonService skeletonService = new SkeletonServiceImpl();
-            skeletonService.generate(generatePath, prefixTemplatePath, reducedTemplatePath, skeletonProperties);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // 全局配置类对象
+        SkeletonProperties skeletonProperties = new SkeletonProperties(propertiesPath, SkeletonConstant.ENCODING_GBK, SkeletonConstant.ENCODING_UTF_8);
+        // **************************************
+
+        // 输出脚手架文件
+        SkeletonService skeletonService = new SkeletonServiceImpl();
+        skeletonService.generate(generatePath, skeletonContext, skeletonProperties);
     }
 }
