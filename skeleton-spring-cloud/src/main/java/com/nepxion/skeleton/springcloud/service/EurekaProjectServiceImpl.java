78cc993fca107d765824ddaca002588c68938154
Nepxion <1394997@qq.com>
2018-01-02 11:44:26 +0800

    重构参数传导方式

--- skeleton-spring-cloud/src/main/java/com/nepxion/skeleton/springcloud/service/EurekaProjectServiceImpl.java
+++ skeleton-spring-cloud/src/main/java/com/nepxion/skeleton/springcloud/service/EurekaProjectServiceImpl.java
@@ -12,6 +12,7 @@ package com.nepxion.skeleton.springcloud.service;
 
 import java.io.IOException;
 
+import com.nepxion.skeleton.engine.context.SkeletonContext;
 import com.nepxion.skeleton.engine.exception.SkeletonException;
 import com.nepxion.skeleton.engine.property.SkeletonProperties;
 import com.nepxion.skeleton.framework.service.SkeletonService;
@@ -24,19 +25,15 @@ import freemarker.template.TemplateException;
 
 public class EurekaProjectServiceImpl implements SkeletonService {
     @Override
-    public void generate(String generatePath, String prefixTemplatePath, String reducedTemplatePath, SkeletonProperties skeletonProperties) throws SkeletonException, TemplateException, IOException {
-        String projectType = "eureka";
-
+    public void generate(String generatePath, SkeletonContext skeletonContext, SkeletonProperties skeletonProperties) throws SkeletonException, TemplateException, IOException {
         // 创建Java类文件到main/java目录下
-        new EurekaApplicationClassGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+        new EurekaApplicationClassGenerator(generatePath, skeletonContext, skeletonProperties).generate();
+        new ApplicationPropertiesGenerator(generatePath, skeletonContext, skeletonProperties).generate();
 
         // 创建文件到main/resources目录下
-        new ApplicationPropertiesGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
-
-        // 创建文件到main/resources目录下
-        new LogbackXmlGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+        new LogbackXmlGenerator(generatePath, skeletonContext, skeletonProperties, "eureka").generate();
 
         // 创建文件到根目录下
-        new PomXmlGenerator(generatePath, projectType, prefixTemplatePath, reducedTemplatePath, skeletonProperties).generate();
+        new PomXmlGenerator(generatePath, skeletonContext, skeletonProperties).generate();
     }
 }
