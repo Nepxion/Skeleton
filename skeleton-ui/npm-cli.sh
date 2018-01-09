914053187dc9e4886789703d628a119948251509
Nepxion <1394997@qq.com>
2018-01-09 19:27:28 +0800

    提交脚本

---
+++ skeleton-ui/npm-cli.sh
@@ -0,0 +1,25 @@
+echo 'on'
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      Nepxion Skeleton                     $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  Nepxion Technologies All Right Reserved                  $'
+echo '$  Copyright(C) 2017                                        $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+echo 'off'
+
+title=Nepxion Skeleton
+color=0a
+# npm install
+
+# npm start
+
+npm run dev
+
+# npm run remote
+
+# npm run build
