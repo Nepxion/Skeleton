2062f8421c762291608e03e36cd14f3c86022c81
Nepxion <1394997@qq.com>
2018-01-11 00:06:26 +0800

    重构类结构

--- skeleton-engine/src/main/java/com/nepxion/skeleton/engine/config/SkeletonConfig.java
+++ skeleton-engine/src/main/java/com/nepxion/skeleton/engine/config/SkeletonConfig.java
@@ -3,10 +3,9 @@ package com.nepxion.skeleton.engine.config;
 /**
  * <p>Title: Nepxion Skeleton</p>
  * <p>Description: Nepxion Skeleton For Freemarker</p>
- * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Copyright: Copyright (c) 2017-2020</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
- * @email 1394997@qq.com
  * @version 1.0
  */
 
