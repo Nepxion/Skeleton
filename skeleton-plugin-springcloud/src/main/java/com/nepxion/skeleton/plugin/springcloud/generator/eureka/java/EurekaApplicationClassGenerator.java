1e1c822246b4bff36f4ba3cffadb5a2629fa77f8
Nepxion <1394997@qq.com>
2018-01-12 14:25:26 +0800

    修改COPYRIGHT

--- skeleton-plugin-springcloud/src/main/java/com/nepxion/skeleton/plugin/springcloud/generator/eureka/java/EurekaApplicationClassGenerator.java
+++ skeleton-plugin-springcloud/src/main/java/com/nepxion/skeleton/plugin/springcloud/generator/eureka/java/EurekaApplicationClassGenerator.java
@@ -3,7 +3,7 @@ package com.nepxion.skeleton.plugin.springcloud.generator.eureka.java;
 /**
  * <p>Title: Nepxion Skeleton</p>
  * <p>Description: Nepxion Skeleton For Freemarker</p>
- * <p>Copyright: Copyright (c) 2017-2020</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
  * @version 1.0
