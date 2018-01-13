7fefdd88a19ba3a47155f7732f372637da32b596
Nepxion <1394997@qq.com>
2018-01-13 21:35:10 +0800

    修复Bug

--- skeleton-engine/src/main/java/com/nepxion/skeleton/engine/util/ZipUtil.java
+++ skeleton-engine/src/main/java/com/nepxion/skeleton/engine/util/ZipUtil.java
@@ -172,6 +172,9 @@ public class ZipUtil {
      */
     private static String buildDestinationZipFilePath(File srcFile, String destPath) {
         if (StringUtils.isEmpty(destPath)) {
+            if (!srcFile.exists()) {
+                throw new RuntimeException("File or directory doesn't exist, path=" + srcFile.getPath());
+            }
             if (srcFile.isDirectory()) {
                 destPath = srcFile.getParent() + File.separator + srcFile.getName() + "." + SkeletonConstant.FILE_ZIP;
             } else {
@@ -181,6 +184,9 @@ public class ZipUtil {
         } else {
             createDestDirectoryIfNecessary(destPath); // 在指定路径不存在的情况下将其创建出来  
             if (destPath.endsWith(File.separator)) {
+                if (!srcFile.exists()) {
+                    throw new RuntimeException("File or directory doesn't exist, path=" + srcFile.getPath());
+                }
                 String fileName = "";
                 if (srcFile.isDirectory()) {
                     fileName = srcFile.getName();
