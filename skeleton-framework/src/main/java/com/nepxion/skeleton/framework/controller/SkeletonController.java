e4fbc9f0f995f0ca6f4dbd4e92db6216cbc21b07
Nepxion <1394997@qq.com>
2018-06-23 22:13:16 +0800

    重构类结构

--- skeleton-framework/src/main/java/com/nepxion/skeleton/framework/controller/SkeletonController.java
+++ skeleton-framework/src/main/java/com/nepxion/skeleton/framework/controller/SkeletonController.java
@@ -76,43 +76,43 @@ public class SkeletonController {
         }
     }
 
-    @RequestMapping(value = "/getPlugins", method = RequestMethod.GET)
+    @RequestMapping(path = "/getPlugins", method = RequestMethod.GET)
     @ApiOperation(value = "获取脚手架插件名列表接口", notes = "获取脚手架插件列表接口", response = List.class, httpMethod = "GET")
     public List<String> getSkeletonPlugins() {
         return skeletonPlugins;
     }
 
-    @RequestMapping(value = "/getMetaData", method = RequestMethod.GET)
+    @RequestMapping(path = "/getMetaData", method = RequestMethod.GET)
     @ApiOperation(value = "获取默认元数据接口", notes = "获取默认界面驱动的元数据接口", response = List.class, httpMethod = "GET")
     public List<SkeletonGroup> getMetaData() {
         return getSkeletonTransport(skeletonDefaultPlugin).getMetaData();
     }
 
-    @RequestMapping(value = "/getMetaData/{skeletonPlugin}", method = RequestMethod.GET)
+    @RequestMapping(path = "/getMetaData/{skeletonPlugin}", method = RequestMethod.GET)
     @ApiOperation(value = "获取元数据接口", notes = "根据脚手架插件名，获取对应的界面驱动的元数据接口", response = List.class, httpMethod = "GET")
     public List<SkeletonGroup> getMetaData(@PathVariable(value = "skeletonPlugin") @ApiParam(value = "脚手架插件名", required = true) String skeletonPlugin) {
         return getSkeletonTransport(skeletonPlugin).getMetaData();
     }
 
-    @RequestMapping(value = "/downloadBytes", method = RequestMethod.POST)
+    @RequestMapping(path = "/downloadBytes", method = RequestMethod.POST)
     @ApiOperation(value = "下载默认脚手架", notes = "下载默认脚手架Zip文件的接口，返回Zip文件的byte数组类型", response = byte[].class, httpMethod = "POST")
     public byte[] downloadBytes(@RequestBody @ApiParam(value = "配置文件内容，可拷贝src/main/resources/config/skeleton-data.properties的内容", required = true) String config) {
         return getSkeletonTransport(skeletonDefaultPlugin).downloadBytes(config);
     }
 
-    @RequestMapping(value = "/downloadBytes/{skeletonPlugin}", method = RequestMethod.POST)
+    @RequestMapping(path = "/downloadBytes/{skeletonPlugin}", method = RequestMethod.POST)
     @ApiOperation(value = "下载脚手架", notes = "根据脚手架插件名，下载脚手架Zip文件的接口，返回Zip文件的byte数组类型", response = byte[].class, httpMethod = "POST")
     public byte[] downloadBytes(@PathVariable(value = "skeletonPlugin") @ApiParam(value = "脚手架插件名", required = true) String skeletonPlugin, @RequestBody @ApiParam(value = "配置文件内容，可拷贝src/main/resources/config/skeleton-data.properties的内容", required = true) String config) {
         return getSkeletonTransport(skeletonPlugin).downloadBytes(config);
     }
 
-    @RequestMapping(value = "/downloadResponse", method = RequestMethod.POST)
+    @RequestMapping(path = "/downloadResponse", method = RequestMethod.POST)
     @ApiOperation(value = "下载默认脚手架", notes = "下载默认脚手架Zip文件的接口，返回Zip文件的ResponseEntity类型", response = ResponseEntity.class, httpMethod = "POST")
     public ResponseEntity<Resource> downloadResponse(@RequestBody @ApiParam(value = "配置文件内容，可拷贝src/main/resources/config/skeleton-data.properties的内容", required = true) String config) {
         return getSkeletonTransport(skeletonDefaultPlugin).downloadResponse(config);
     }
 
-    @RequestMapping(value = "/downloadResponse/{skeletonPlugin}", method = RequestMethod.POST)
+    @RequestMapping(path = "/downloadResponse/{skeletonPlugin}", method = RequestMethod.POST)
     @ApiOperation(value = "下载脚手架", notes = "根据脚手架插件名，下载脚手架Zip文件的接口，返回Zip文件的ResponseEntity类型", response = ResponseEntity.class, httpMethod = "POST")
     public ResponseEntity<Resource> downloadResponse(@PathVariable(value = "skeletonPlugin") @ApiParam(value = "脚手架插件名", required = true) String skeletonPlugin, @RequestBody @ApiParam(value = "配置文件内容，可拷贝src/main/resources/config/skeleton-data.properties的内容", required = true) String config) {
         return getSkeletonTransport(skeletonPlugin).downloadResponse(config);
