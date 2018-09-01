686a7052c1586fdf013b47eb411ec4c022362252
Nepxion <1394997@qq.com>
2018-09-01 19:33:09 +0800

    升级版本到2.0.1

--- skeleton-engine/src/main/java/com/nepxion/skeleton/engine/property/SkeletonProperties.java
+++ skeleton-engine/src/main/java/com/nepxion/skeleton/engine/property/SkeletonProperties.java
@@ -34,7 +34,7 @@ public class SkeletonProperties implements Serializable {
         System.out.println("╚══╗║╚╝╣║═╣║║║═╣║║║╔╗║╔╗║");
         System.out.println("║╚═╝║╔╗╣║═╣╚╣║═╣║╚╣╚╝║║║║");
         System.out.println("╚═══╩╝╚╩══╩═╩══╝╚═╩══╩╝╚╝");
-        System.out.println("Nepxion Skeleton  v2.0.0");
+        System.out.println("Nepxion Skeleton  v2.0.1");
         System.out.println("");
     }
 
